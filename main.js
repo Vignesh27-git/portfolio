e79ad681be80dc32aff078138e8c1fcdd72325af
Vignesh M <vigneshchary35@gmail.com>
2026-03-13 13:36:16 +0530

    Added fliter section

---
+++ main.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function () {
+  $('.menu-toggler').on('click', function(){
+      $(this).toggleClass('open');
+      $('.top-nav').toggleClass('open');
+  });
+
+  $('.top-nav .nav-link').on('click', function(){
+      $('.menu-toggler').toggleClass('open');
+      $('.top-nav').toggleClass('open');
+  });
+
+  $('nav a[href*="#]').on('click', function(){
+      $('html,body').animate({
+          scrollTop: $($(this).attr('href')).offset().top-100
+      }, 2000);
+  });
+
+  $('#up').on('click', function(){
+      $('html,body').animate({
+          scrollTop: 0 } ,2000);
+  });
+
+  AOS.init({
+      easing: 'ease',
+      duration: 1800
+  });
+});
+
+
+function filterProjects(category) {
+
+  const projects = document.querySelectorAll(".portfolio-item");
+
+  projects.forEach(project => {
+
+    if (category === "all" || project.dataset.category === category) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+
+  });
+
+}
