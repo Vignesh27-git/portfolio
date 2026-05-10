83d44b471bc624b93de3fdcdc87ac9a561355337
Vignesh M <vigneshchary35@gmail.com>
2026-05-10 17:36:16 +0530

    git

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { SiTypescript, SiDocker } from "react-icons/si";
+
 import Javascript from "../../Assets/TechIcons/Javascript.svg";
 import Node from "../../Assets/TechIcons/Node.svg";
 import ReactIcon from "../../Assets/TechIcons/React.svg";
@@ -11,39 +12,47 @@ import Tailwind from "../../Assets/TechIcons/Tailwind.svg";
 import MUI from "../../Assets/TechIcons/MUI.svg";
 import Postman from "../../Assets/TechIcons/Postman.svg";
 
-
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      
       <Col xs={4} md={2} className="tech-icons">
         <img src={Javascript} alt="javascript" />
-        <div className="tech-icons-text">Javascript</div>
+        <div className="tech-icons-text">JavaScript</div>
       </Col>
+
+      {/* ✅ FIXED: TypeScript */}
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Typescript} alt="typescript" />
-        <div className="tech-icons-text">Javascript</div>
+        <SiTypescript size={40} />
+        <div className="tech-icons-text">TypeScript</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={ReactIcon} alt="react" />
-        <div className="tech-icons-text">React.Js</div>
+        <div className="tech-icons-text">React.js</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Redux} alt="redux" />
         <div className="tech-icons-text">Redux</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Git} alt="git" />
         <div className="tech-icons-text">Git</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Firebase} alt="firebase" />
         <div className="tech-icons-text">Firebase</div>
       </Col>
-      
+
+      {/* ✅ FIXED: Docker */}
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Docker} alt="docker" />
+        <SiDocker size={40} />
         <div className="tech-icons-text">Docker</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Tailwind} alt="tailwind" />
         <div className="tech-icons-text">Tailwind CSS</div>
@@ -55,7 +64,7 @@ function Techstack() {
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Postman} alt="Postman" />
+        <img src={Postman} alt="postman" />
         <div className="tech-icons-text">Postman</div>
       </Col>
 
