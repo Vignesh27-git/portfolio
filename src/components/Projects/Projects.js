31221eef15678b5927d7b802f97ad94b9b5ecd76
Vignesh M <vigneshchary35@gmail.com>
2026-05-21 13:12:45 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import todolist from "../../Assets/Projects/todolist.png";
 import shoping from "../../Assets/Projects/shoping.png";
